94c066e89430dbad5eb0ecd68542b3c06f1c1013
Adrian Nembach <adrian.nembach@knime.com>
2021-05-03 11:21:46 +0200

    AP-15900: Project setup
    AP-15900 (Revise RowCursor)

---
+++ org.knime.core.table.tests/src/test/java/org/knime/core/table/DummyTest.java
@@ -0,0 +1,35 @@
+/* ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright by KNIME AG, Zurich, Switzerland
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.com
+ * email: contact@knime.com
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Created on 16 Apr 2021 by Marc
+ */
+package org.knime.core.table;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Marc
+ */
+public class DummyTest {
+
+	@Test
+    public void testAccesses() {
+	}
+
+}
