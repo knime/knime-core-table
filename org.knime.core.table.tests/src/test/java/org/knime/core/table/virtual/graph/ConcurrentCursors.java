768cbef7a2221d19ded4079ad13624054f4b9597
tpietzsch <tobias.pietzsch@gmail.com>
2022-01-20 16:31:31 +0100

    WIP outputting ordered rows

--- org.knime.core.table.tests/src/test/java/org/knime/core/table/virtual/graph/ConcurrentCursors.java
+++ org.knime.core.table.tests/src/test/java/org/knime/core/table/virtual/graph/ConcurrentCursors.java
@@ -9,23 +9,23 @@ public class ConcurrentCursors
 {
 	public static void main( String[] args )
 	{
-		final List< Row > rows = generateData( 100, 0.5, 10, 1000 );
+		final List< Row > rows = generateData( 10, 0.5, 10, 1000 );
 		for ( Row row : rows )
 			System.out.println( row );
 
-		Collections.shuffle( rows );
+		Collections.shuffle( rows, new Random( 2l ) );
 		OrderList orderList = new OrderList();
 		for ( Row row : rows )
 		{
-			System.out.println();
-			System.out.println();
-			System.out.println( "row = " + row );
-			System.out.println();
+//			System.out.println();
+//			System.out.println();
+//			System.out.println( "row = " + row );
+//			System.out.println();
 			if ( row.skipped )
 				orderList.skip( row.rowIndex );
 			else
 				orderList.accept( row.rowIndex, row );
-			System.out.println( "orderList = \n" + orderList );
+//			System.out.println( "orderList = \n" + orderList );
 		}
 	}
 
@@ -62,7 +62,7 @@ public class ConcurrentCursors
 		{
 			return "Row{" +
 					rowIndex +
-					(skipped ? " (skipped) " : " ")+
+					(skipped ? " (skipped)" : "")+
 					", t=" + processingTime +
 					'}';
 		}
@@ -83,14 +83,13 @@ public class ConcurrentCursors
 	{
 		int firstRowIndex;
 		int lastRowIndex;
-		List<Row> buffers;
+		final List<Row> buffers = new ArrayList<>();
 
 		// constructor for a buffered output row
 		OrderEntry( int index, Row buffer )
 		{
 			this.firstRowIndex = index;
 			this.lastRowIndex = index;
-			this.buffers = new ArrayList<>();
 			this.buffers.add( buffer );
 		}
 
@@ -99,7 +98,6 @@ public class ConcurrentCursors
 		{
 			this.firstRowIndex = index;
 			this.lastRowIndex = index;
-			this.buffers = null;
 		}
 
 		@Override
@@ -115,117 +113,135 @@ public class ConcurrentCursors
 
 	static class OrderList
 	{
-		ArrayList< OrderEntry > entries = new ArrayList<>();
+		private final OutputQueue outputQueue = new OutputQueue();
 
-		void accept( final int rowIndex, final Row row ) {
+		private int pendingIndex = 0;
+
+		private final ArrayList<OrderEntry> entries = new ArrayList<>();
+		// TODO: Would it be more efficient to replace this with a (growable) RingBuffer?
+		// 		(But start by profiling...)
+
+		void accept(final int rowIndex, final Row row) {
+
+			if (tryOutput(rowIndex, row))
+				return;
 
 			// find the entry before which this index will be inserted
 			// by comparing with firstRowIndex
-			int i = findInsertionIndex( rowIndex );
+			int i = findInsertionIndex(rowIndex);
 
 			// can rowIndex be prepended to the OrderEntry at index i?
-			if ( i < entries.size() ) {
-				final OrderEntry entry = entries.get( i );
-				if ( entry.firstRowIndex == rowIndex + 1 )
-				{
+			if (i < entries.size()) {
+				final OrderEntry entry = entries.get(i);
+				if (entry.firstRowIndex == rowIndex + 1) {
 					entry.firstRowIndex = rowIndex;
-					if ( entry.buffers == null )
-						entry.buffers = new ArrayList<>();
-					entry.buffers.add( 0, row );
-					// TODO: check whether we can advance the output
-					tryMerge( i - 1 );
+					entry.buffers.add(0, row);
+					tryMerge(i - 1);
 					return;
 				}
 			}
 
 			// can rowIndex be appended to the OrderEntry at index i-1?
-			if ( i > 0) {
-				final OrderEntry entry = entries.get( i - 1 );
-				if ( entry.lastRowIndex == rowIndex - 1 )
-				{
+			if (i > 0) {
+				final OrderEntry entry = entries.get(i - 1);
+				if (entry.lastRowIndex == rowIndex - 1) {
 					entry.lastRowIndex = rowIndex;
-					if ( entry.buffers == null )
-						entry.buffers = new ArrayList<>();
-					entry.buffers.add( row );
-					tryMerge( i );
+					entry.buffers.add(row);
+					tryMerge(i);
 					return;
 				}
 			}
 
 			// insert a new entry at i
-			entries.add( i, new OrderEntry( rowIndex, row ) );
+			entries.add(i, new OrderEntry(rowIndex, row));
 		}
 
-		void skip( int rowIndex ) {
+		void skip(int rowIndex) {
+
+			if (tryOutput(rowIndex))
+				return;
+
 			// find the entry before which this index will be inserted
 			// by comparing with firstRowIndex
-			int i = findInsertionIndex( rowIndex );
+			int i = findInsertionIndex(rowIndex);
 
 			// can rowIndex be prepended to the OrderEntry at index i?
-			if ( i < entries.size() ) {
-				final OrderEntry entry = entries.get( i );
-				if ( entry.firstRowIndex == rowIndex + 1 )
-				{
+			if (i < entries.size()) {
+				final OrderEntry entry = entries.get(i);
+				if (entry.firstRowIndex == rowIndex + 1) {
 					entry.firstRowIndex = rowIndex;
-					// TODO: check whether we can advance the output
-					tryMerge( i - 1 );
+					tryMerge(i - 1);
 					return;
 				}
 			}
 
 			// can rowIndex be appended to the OrderEntry at index i-1?
-			if ( i > 0) {
-				final OrderEntry entry = entries.get( i - 1 );
-				if ( entry.lastRowIndex == rowIndex - 1 )
-				{
+			if (i > 0) {
+				final OrderEntry entry = entries.get(i - 1);
+				if (entry.lastRowIndex == rowIndex - 1) {
 					entry.lastRowIndex = rowIndex;
-					tryMerge( i );
+					tryMerge(i);
 					return;
 				}
 			}
 
 			// insert a new entry at i
-			entries.add( i, new OrderEntry( rowIndex ) );
+			entries.add(i, new OrderEntry(rowIndex));
 		}
 
 		// try to merge entries at i and i + 1
-		private void tryMerge( int i )
-		{
-			if ( i >= 0 && i + 1 < entries.size() )
-			{
-				final OrderEntry e1 = entries.get( i );
-				final OrderEntry e2 = entries.get( i + 1 );
-				if ( e1.lastRowIndex + 1 == e2.firstRowIndex )
-				{
+		private void tryMerge(int i) {
+			if (i >= 0 && i + 1 < entries.size()) {
+				final OrderEntry e1 = entries.get(i);
+				final OrderEntry e2 = entries.get(i + 1);
+				if (e1.lastRowIndex + 1 == e2.firstRowIndex) {
 					e1.lastRowIndex = e2.lastRowIndex;
-					if (e1.buffers != null )
-					{
-						if ( e2.buffers != null )
-						{
-							e1.buffers.addAll( e2.buffers );
-						}
-					} else {
-						e1.buffers = e2.buffers;
-					}
-					entries.remove( i + 1 );
+					e1.buffers.addAll(e2.buffers);
+					entries.remove(i + 1);
 				}
 			}
 		}
 
 		//   TODO: replace by binary search
-		private int findInsertionIndex( int rowIndex )
-		{
-			for ( int i = 0; i < entries.size(); i++ )
-			{
-				if ( rowIndex < entries.get( i ).firstRowIndex )
+		private int findInsertionIndex(int rowIndex) {
+			for (int i = 0; i < entries.size(); i++) {
+				if (rowIndex < entries.get(i).firstRowIndex) {
 					return i;
+				}
 			}
 			return entries.size();
 		}
 
+		private boolean tryOutput(final int rowIndex) {
+			return tryOutput(rowIndex, null);
+		}
+
+		private boolean tryOutput(final int rowIndex, final Row row) {
+			if (rowIndex != pendingIndex) {
+				return false;
+			}
+
+			if (row != null) {
+				outputQueue.accept(row);
+			} // else skip rowIndex
+
+			++pendingIndex;
+
+			if (!entries.isEmpty()) {
+				if (entries.get(0).firstRowIndex == pendingIndex) {
+					final OrderEntry entry = entries.remove(0);
+					entry.buffers.forEach(outputQueue::accept);
+					pendingIndex = entry.lastRowIndex + 1;
+				}
+			}
+
+			// TODO: set new pendingIndex to RowQueue
+
+			return true;
+		}
+
 		@Override
-		public String toString()
-		{
+		public String toString() {
 			final StringBuffer sb = new StringBuffer();
 			for (OrderEntry entry : entries) {
 				sb.append("  " + entry + "\n");
