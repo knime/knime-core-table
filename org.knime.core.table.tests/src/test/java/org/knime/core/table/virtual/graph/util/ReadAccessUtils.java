c02a2cb17ebbd0967e8b866bd9710f80cb227751
tpietzsch <tobias.pietzsch@gmail.com>
2022-01-20 22:15:50 +0100

    Debug output for BooleanReadAccess

--- org.knime.core.table.tests/src/test/java/org/knime/core/table/virtual/graph/util/ReadAccessUtils.java
+++ org.knime.core.table.tests/src/test/java/org/knime/core/table/virtual/graph/util/ReadAccessUtils.java
@@ -1,5 +1,6 @@
 package org.knime.core.table.virtual.graph.util;
 
+import org.knime.core.table.access.BooleanAccess;
 import org.knime.core.table.access.DoubleAccess;
 import org.knime.core.table.access.IntAccess;
 import org.knime.core.table.access.ReadAccess;
@@ -8,14 +9,17 @@ import org.knime.core.table.access.StringAccess;
 public class ReadAccessUtils {
     public  static String toString(final ReadAccess access) {
         if (access instanceof IntAccess.IntReadAccess) {
-            final IntAccess.IntReadAccess a = (IntAccess.IntReadAccess)access;
+            final var a = (IntAccess.IntReadAccess)access;
             return "(int) " + (a.isMissing() ? "-missing-" : a.getIntValue());
         } else if (access instanceof StringAccess.StringReadAccess) {
-            final StringAccess.StringReadAccess a = (StringAccess.StringReadAccess)access;
+            final var a = (StringAccess.StringReadAccess)access;
             return "(string) " + (a.isMissing() ? "-missing-" : a.getStringValue());
         } else if (access instanceof DoubleAccess.DoubleReadAccess) {
-            final DoubleAccess.DoubleReadAccess a = (DoubleAccess.DoubleReadAccess)access;
+            final var a = (DoubleAccess.DoubleReadAccess)access;
             return "(double) " + (a.isMissing() ? "-missing-" : a.getDoubleValue());
+        } else if (access instanceof BooleanAccess.BooleanReadAccess) {
+            final var a = (BooleanAccess.BooleanReadAccess)access;
+            return "(boolean) " + (a.isMissing() ? "-missing-" : a.getBooleanValue());
         }
         throw new UnsupportedOperationException("not implemented yet");
     }
