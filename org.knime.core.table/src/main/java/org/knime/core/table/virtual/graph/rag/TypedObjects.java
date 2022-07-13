a685b76cb130221625524326f5fe3d1fd7a432cd
tpietzsch <tobias.pietzsch@gmail.com>
2022-07-13 13:30:17 +0200

    AP-18627: Add method to insert RagGraoh edge at specific index

--- org.knime.core.table/src/main/java/org/knime/core/table/virtual/graph/rag/TypedObjects.java
+++ org.knime.core.table/src/main/java/org/knime/core/table/virtual/graph/rag/TypedObjects.java
@@ -34,6 +34,27 @@ class TypedObjects<T extends Enum<T>, O extends Typed<T>> {
     }
 
     /**
+     * Returns the index of the first occurrence of the specified {@code object}
+     * in the list for the object's {@link Typed#type()}, or -1 if there is no
+     * such index.
+     */
+    public int indexOf(O object) {
+        return objects.get(object.type().ordinal() + 1).indexOf(object);
+    }
+
+    /**
+     * Inserts the specified {@code object} at the specified position in the
+     * list for the object's {@link Typed#type()}. Shifts the element currently
+     * at that position (if any) and any subsequent elements to the right (adds
+     * one to their indices). The {@code object} is also added to the
+     * all-objects list.
+     */
+    public boolean add(int index, O object) {
+        objects.get(object.type().ordinal() + 1).add(index, object);
+        return objects.get(0).add(object);
+    }
+
+    /**
      * Add {@code object} to the all-objects list, and to the list for the object's
      * {@link Typed#type()}.
      */
