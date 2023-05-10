5eee0d77d487fb3bf20fa821c0cf2d954ed2ca67
tpietzsch <tobias.pietzsch@gmail.com>
2023-05-10 15:52:28 +0200

    Techdebt: Add javadoc

--- org.knime.core.table/src/main/java/org/knime/core/table/virtual/graph/rag/WrapperTransformSpec.java
+++ org.knime.core.table/src/main/java/org/knime/core/table/virtual/graph/rag/WrapperTransformSpec.java
@@ -4,7 +4,7 @@ import org.knime.core.table.virtual.spec.TableTransformSpec;
 
 /**
  * Artificial {@link TableTransformSpec} that represents one input table of a
- * CONCATENATE operation Is created when the RagGraph is build.
+ * CONCATENATE operation. Is created when the RagGraph is build.
  */
 public class WrapperTransformSpec implements TableTransformSpec {
 
