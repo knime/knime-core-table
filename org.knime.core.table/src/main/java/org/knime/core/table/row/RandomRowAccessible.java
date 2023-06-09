dd68d06f138ed5e662bdbe4fa70aaab5179cc5d9
tpietzsch <tobias.pietzsch@gmail.com>
2023-06-09 14:48:11 +0200

    AP-20210: Add javadoc

--- org.knime.core.table/src/main/java/org/knime/core/table/row/RandomRowAccessible.java
+++ org.knime.core.table/src/main/java/org/knime/core/table/row/RandomRowAccessible.java
@@ -43,6 +43,15 @@ public interface RandomRowAccessible extends LookaheadRowAccessible {
      */
     interface RandomAccessCursor<A> extends LookaheadCursor<A> {
 
+        /**
+         * Move this cursor to the specified {@code row}.
+         * <p>
+         * The effect of {@code moveTo(i)} is that this cursor's {@link #access} refers the same data as if calling
+         * {@link #forward} {@code i+1} times on a new cursor.
+         *
+         * @param row index of the row to move to
+         * @throws IndexOutOfBoundsException if {@code row<0} or the table contains less than {@code row-1} rows.
+         */
         void moveTo(long row);
 
     }
