6535c6ced573d235877008bf4cae5a7c0cad52a0
tpietzsch <tobias.pietzsch@gmail.com>
2023-06-27 23:46:56 +0200

    AP-20369: Add javadoc

--- org.knime.core.table/src/main/java/org/knime/core/table/virtual/graph/exec/RandomAccessNodeImp.java
+++ org.knime.core.table/src/main/java/org/knime/core/table/virtual/graph/exec/RandomAccessNodeImp.java
@@ -1,12 +1,14 @@
 package org.knime.core.table.virtual.graph.exec;
 
-import org.knime.core.table.virtual.graph.exec.AbstractNodeImp;
-
 interface RandomAccessNodeImp extends AbstractNodeImp {
 
     /**
-     * TODO javadoc
-     * @param row
+     * Recursively call {@link #moveTo} on predecessors.
+     * Perform any necessary predecessor switching and access buffering. For
+     * example, {@link RandomAccessNodeImpConcatenate concatenate} switches to
+     * the predecessor containing the specified {@code row}.
+     *
+     * @param row index of the row to move to
      */
     void moveTo(long row);
 }
