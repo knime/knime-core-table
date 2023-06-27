e296935c685a6192c2ea3fee9e1f0b581160b01c
tpietzsch <tobias.pietzsch@gmail.com>
2023-06-27 23:37:56 +0200

    AP-20369: Remove unnecessary code

--- org.knime.core.table/src/main/java/org/knime/core/table/virtual/graph/exec/NodeImpConcatenate.java
+++ org.knime.core.table/src/main/java/org/knime/core/table/virtual/graph/exec/NodeImpConcatenate.java
@@ -75,11 +75,6 @@ class NodeImpConcatenate implements NodeImp {
                 outputs[i].setDelegateAccess(access);
             }
             linkedPredecessor = predecessors[predecessorIndex];
-        } else {
-            // TODO (TP) Is this necessary? Why not just do nothing (no check either)?
-            for (DelegatingReadAccesses.DelegatingReadAccess output : outputs) {
-                output.setDelegateAccess(MissingAccesses.getMissingAccess(output.getDataSpec()));
-            }
         }
     }
 
