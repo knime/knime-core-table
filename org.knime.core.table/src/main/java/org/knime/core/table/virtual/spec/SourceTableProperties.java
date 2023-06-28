6afecab7890b86f1285c14a469e09939f19565a8
tpietzsch <tobias.pietzsch@gmail.com>
2023-06-28 22:06:15 +0200

    AP-20369: Use CursorType enum instead of supportsLookahead and supportsRandomAccess flags

--- org.knime.core.table/src/main/java/org/knime/core/table/virtual/spec/SourceTableProperties.java
+++ org.knime.core.table/src/main/java/org/knime/core/table/virtual/spec/SourceTableProperties.java
@@ -1,5 +1,6 @@
 package org.knime.core.table.virtual.spec;
 
+import org.knime.core.table.cursor.Cursor;
 import org.knime.core.table.cursor.LookaheadCursor;
 import org.knime.core.table.cursor.RandomAccessCursor;
 import org.knime.core.table.row.LookaheadRowAccessible;
@@ -16,26 +17,86 @@ import org.knime.core.table.schema.ColumnarSchema;
  */
 public class SourceTableProperties {
 
+    /**
+     * Cursor capabilities.
+     */
+    public enum CursorType {
+        /**
+         * At least normal {@link Cursor}.
+         */
+        BASIC(false, false),
+
+        /**
+         * At least {@link LookaheadCursor}.
+         */
+        LOOKAHEAD(true, false),
+
+        /**
+         * At least {@link RandomAccessCursor}.
+         */
+        RANDOMACCESS(true, true);
+
+        /**
+         * Derive cursor capabilities from the type of {@code source}
+         *
+         * @param source {@code RowAccessible} for which to determine cursor capabilities
+         * @return the capabilities of {@code source}'s cursors.
+         */
+        public static CursorType of(final RowAccessible source) {
+            if (source instanceof RandomRowAccessible) {
+                return RANDOMACCESS;
+            } else if (source instanceof LookaheadRowAccessible) {
+                return LOOKAHEAD;
+            } else {
+                return BASIC;
+            }
+        }
+
+        /**
+         * Whether this {@code CursorType} supports {@link LookaheadCursor}.
+         *
+         * @return {@code true} if this cursor type supports {@code LookaheadCursor}
+         */
+        public boolean supportsLookahead() {
+            return supportsLookahead;
+        }
+
+        /**
+         * Whether this {@code CursorType} supports {@link RandomAccessCursor}.
+         *
+         * @return {@code true} if this source supports {@code RandomAccessCursor}s
+         */
+        public boolean supportsRandomAccess() {
+            return supportsRandomAccess;
+        }
+
+        private final boolean supportsLookahead;
+
+        private final boolean supportsRandomAccess;
+
+        CursorType(final boolean lookahead, final boolean randomAccess) {
+            this.supportsLookahead = lookahead;
+            this.supportsRandomAccess = randomAccess;
+        }
+    }
+
     private final ColumnarSchema m_schema;
 
-    private final boolean m_lookahead;
-
-    private final boolean m_randomAccess;
+    private final CursorType m_cursorType;
 
     private final long m_numRows;
 
     public SourceTableProperties(final RowAccessible source) {
-        this(source.getSchema(), source instanceof LookaheadRowAccessible, source instanceof RandomRowAccessible, source.size());
+        this(source.getSchema(), CursorType.of(source), source.size());
     }
 
-    public SourceTableProperties(final ColumnarSchema schema, final boolean lookahead) {
-        this(schema, lookahead, false, -1);
+    public SourceTableProperties(final ColumnarSchema schema, final CursorType cursorType) {
+        this(schema, cursorType, -1);
     }
 
-    public SourceTableProperties(final ColumnarSchema schema, final boolean lookahead, final boolean randomAccess, final long numRows) {
+    public SourceTableProperties(final ColumnarSchema schema, final CursorType cursorType, final long numRows) {
         this.m_schema = schema;
-        this.m_lookahead = lookahead;
-        this.m_randomAccess = randomAccess;
+        this.m_cursorType = cursorType;
         this.m_numRows = numRows;
     }
 
@@ -66,7 +127,7 @@ public class SourceTableProperties {
      * @return {@code true} if this source supports {@code LookaheadCursor}s
      */
     public boolean supportsLookahead() {
-        return m_lookahead;
+        return m_cursorType.supportsLookahead();
     }
 
     /**
@@ -76,12 +137,19 @@ public class SourceTableProperties {
      * @return {@code true} if this source supports {@code RandomAccessCursor}s
      */
     public boolean supportsRandomAccess() {
-        return m_randomAccess;
+        return m_cursorType.supportsRandomAccess();
     }
 
     /**
-     * TODO javadoc
-     * @return
+     * Get the number of rows in the source table, if it is known.
+     * <p>
+     * Some tables do not know how many rows they contain. Examples would be streaming tables, or virtual tables which
+     * contain row filters, where the number of rows depends on the data in the rows. In this case, {@code size()<0}
+     * indicates that the number of rows is unknown.
+     * <p>
+     * The default implementation returns {@code -1} to indicate that the number of rows is unknown.
+     *
+     * @return the number of rows, or a negative number if the number of rows is unknown.
      */
     public long numRows()
     {
@@ -89,8 +157,13 @@ public class SourceTableProperties {
     }
 
     /**
-     * TODO javadoc
-     * @return
+     * Whether the source table knows how many rows it contains.
+     * <p>
+     * Some tables do not know how many rows they contain. Examples would be streaming tables, or virtual tables which
+     * contain row filters, where the number of rows depends on the data in the rows.
+     * <p>
+     *
+     * @return {@code true}, if the source table knows how many rows it contains.
      */
     public boolean knowsNumRows()
     {
