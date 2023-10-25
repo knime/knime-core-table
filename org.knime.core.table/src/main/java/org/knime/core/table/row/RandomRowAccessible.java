e0570b02ee6a4178aa0584da2ff2c007f5a3d863
tpietzsch <tobias.pietzsch@gmail.com>
2023-10-25 11:33:34 +0200

    AP-20210: Move RandomAccessCursor to separate file

--- org.knime.core.table/src/main/java/org/knime/core/table/row/RandomRowAccessible.java
+++ org.knime.core.table/src/main/java/org/knime/core/table/row/RandomRowAccessible.java
@@ -20,7 +20,7 @@
  */
 package org.knime.core.table.row;
 
-import org.knime.core.table.cursor.LookaheadCursor;
+import org.knime.core.table.cursor.RandomAccessCursor;
 
 /**
  * {@link LookaheadRowAccessible} whose cursors also support random access via the
@@ -35,24 +35,4 @@ public interface RandomRowAccessible extends LookaheadRowAccessible {
 
     @Override
     RandomAccessCursor<ReadAccessRow> createCursor(Selection selection);
-
-    /**
-     * Cursor that provides random access via the {@link #moveTo(long)} method.
-     *
-     * @author Adrian Nembach, KNIME GmbH, Konstanz, Germany
-     */
-    interface RandomAccessCursor<A> extends LookaheadCursor<A> {
-
-        /**
-         * Move this cursor to the specified {@code row}.
-         * <p>
-         * The effect of {@code moveTo(i)} is that this cursor's {@link #access} refers the same data as if calling
-         * {@link #forward} {@code i+1} times on a new cursor.
-         *
-         * @param row index of the row to move to
-         * @throws IndexOutOfBoundsException if {@code row<0} or the table contains less than {@code row-1} rows.
-         */
-        void moveTo(long row);
-
-    }
 }
