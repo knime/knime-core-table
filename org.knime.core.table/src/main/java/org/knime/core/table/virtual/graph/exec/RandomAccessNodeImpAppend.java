30a026a7eb968b29bf9958c8521e23d0ebf7f18d
tpietzsch <tobias.pietzsch@gmail.com>
2024-06-27 12:23:20 +0200

    WIP Simplify NodeImpAppend

--- org.knime.core.table/src/main/java/org/knime/core/table/virtual/graph/exec/RandomAccessNodeImpAppend.java
+++ org.knime.core.table/src/main/java/org/knime/core/table/virtual/graph/exec/RandomAccessNodeImpAppend.java
@@ -57,42 +57,9 @@ class RandomAccessNodeImpAppend implements RandomAccessNodeImp {
 
     private final AccessImp[] inputs;
 
-    private final DelegatingReadAccesses.DelegatingReadAccess[] outputs;
+    private final ReadAccess[] outputs;
 
     private final RandomAccessNodeImp[] predecessors;
-
-    private final int[][] predecessorOutputIndices;
-
-    private final long[] predecessorSizes;
-
-    /*
-     * We split the input row range into sections, such that in each section the
-     * same predecessors are active (can provide rows). When moving between
-     * sections some predecessor accesses have to be switched to missing or
-     * re-linked.
-     */
-
-    /**
-     * Sequence of start row indices of sections.
-     * First element is 0, last element is numRows (of whole table).
-     */
-    private final long[] sectionStarts;
-
-    /**
-     * First row index in current section. (inclusive)
-     */
-    private long sectionFromRow = -1;
-
-    /**
-     * Last row index in current section. (exclusive)
-     */
-    private long sectionToRow = -1;
-
-    /**
-     * Which predecessors are currently linked to accesses.
-     * (Accesses for the others are set to missing.)
-     */
-    private final boolean[] linked;
 
     /**
      * @param predecessorOutputIndices {@code predecessorOutputIndices[i]} is the list of output indices to switch to
@@ -101,57 +68,17 @@ class RandomAccessNodeImpAppend implements RandomAccessNodeImp {
     RandomAccessNodeImpAppend(//
         final AccessImp[] inputs, //
         final RandomAccessNodeImp[] predecessors, //
-        final int[][] predecessorOutputIndices, //
-        final long[] predecessorSizes) {
+        final int[][] predecessorOutputIndices, // TODO (TP): remove
+        final long[] predecessorSizes) { // TODO (TP): remove
 
         this.inputs = inputs;
-        outputs = new DelegatingReadAccesses.DelegatingReadAccess[inputs.length];
-
+        outputs = new ReadAccess[inputs.length];
         this.predecessors = predecessors;
-        this.predecessorOutputIndices = predecessorOutputIndices;
-        this.predecessorSizes = predecessorSizes;
-        sectionStarts = LongStream.concat( //
-                LongStream.of(0), LongStream.of(predecessorSizes) //
-        ).sorted().distinct().toArray();
-        linked = new boolean[predecessors.length];
     }
 
     @Override
     public ReadAccess getOutput(final int i) {
         return outputs[i];
-    }
-
-    /**
-     * Point our delegate {@code outputs} to predecessor outputs or to missing
-     * access, depending on which section we are in.
-     *
-     * @param s index of section to link
-     */
-    private void link(final int s) {
-
-        sectionFromRow = sectionStarts[s];
-        sectionToRow = sectionStarts[s + 1];
-
-        for (int p = 0; p < predecessors.length; p++) {
-            final boolean currentlyActive = linked[p];
-            final boolean shouldBeActive = predecessorSizes[p] >= sectionToRow;
-            if ( currentlyActive && !shouldBeActive )
-            {
-                for (int i : predecessorOutputIndices[p]) {
-                    final DelegatingReadAccesses.DelegatingReadAccess output = outputs[i];
-                    output.setDelegateAccess(MissingAccesses.getMissingAccess(output.getDataSpec()));
-                }
-                linked[ p ] = false;
-            }
-            else if ( !currentlyActive && shouldBeActive )
-            {
-                for (int i : predecessorOutputIndices[p]) {
-                    final DelegatingReadAccesses.DelegatingReadAccess output = outputs[i];
-                    output.setDelegateAccess(inputs[i].getReadAccess());
-                }
-                linked[ p ] = true;
-            }
-        }
     }
 
     @Override
@@ -160,27 +87,14 @@ class RandomAccessNodeImpAppend implements RandomAccessNodeImp {
             predecessor.create();
         }
         for (int i = 0; i < inputs.length; i++) {
-            final ReadAccess access = inputs[i].getReadAccess();
-            outputs[i] = DelegatingReadAccesses.createDelegatingAccess(access.getDataSpec());
+            outputs[i] = inputs[i].getReadAccess();
         }
     }
 
     @Override
     public void moveTo(final long row) {
-        // NB no bounds checking here, this is done in CapRandomAccessCursor
-        if (row < sectionFromRow || row >= sectionToRow) {
-            // we are moving to a different section
-            int s = Arrays.binarySearch(sectionStarts, row);
-            if (s < 0) {
-                s = -s - 2;
-            }
-            link( s );
-        }
-
-        for (int i = 0; i < predecessors.length; i++) {
-            if (linked[i]) {
-                predecessors[i].moveTo(row);
-            }
+        for (RandomAccessNodeImp predecessor : predecessors) {
+            predecessor.moveTo(row);
         }
     }
 
--- org.knime.core.table/src/main/java/org/knime/core/table/virtual/graph/exec/SequentialNodeImpAppend.java
+++ org.knime.core.table/src/main/java/org/knime/core/table/virtual/graph/exec/SequentialNodeImpAppend.java
@@ -54,25 +54,19 @@ import org.knime.core.table.access.ReadAccess;
 class SequentialNodeImpAppend implements SequentialNodeImp {
     private final AccessImp[] inputs;
 
-    private final DelegatingReadAccesses.DelegatingReadAccess[] outputs;
+    private final ReadAccess[] outputs;
 
     private final SequentialNodeImp[] predecessors;
-
-    private final int[][] predecessorOutputIndices;
-
-    private final boolean[] exhausted;
 
     /**
      * @param predecessorOutputIndices {@code predecessorOutputIndices[i]} is the list
      *                                 of output indices to switch to missing when the i-th predecessor is exhausted.
      */
-    SequentialNodeImpAppend(final AccessImp[] inputs, final SequentialNodeImp[] predecessors, final int[][] predecessorOutputIndices) {
+    SequentialNodeImpAppend(final AccessImp[] inputs, final SequentialNodeImp[] predecessors,
+            final int[][] predecessorOutputIndices) { // TODO (TP): remove
         this.inputs = inputs;
-        outputs = new DelegatingReadAccesses.DelegatingReadAccess[inputs.length];
-
+        outputs = new ReadAccess[inputs.length];
         this.predecessors = predecessors;
-        this.predecessorOutputIndices = predecessorOutputIndices;
-        exhausted = new boolean[predecessors.length];
     }
 
     @Override
@@ -80,39 +74,22 @@ class SequentialNodeImpAppend implements SequentialNodeImp {
         return outputs[i];
     }
 
-    private void link() {
-        for (int i = 0; i < inputs.length; i++) {
-            final ReadAccess access = inputs[i].getReadAccess();
-            final DelegatingReadAccesses.DelegatingReadAccess delegated =
-                    DelegatingReadAccesses.createDelegatingAccess(access.getDataSpec());
-            delegated.setDelegateAccess(access);
-            outputs[i] = delegated;
-        }
-    }
-
     @Override
     public void create() {
         for (SequentialNodeImp predecessor : predecessors) {
             predecessor.create();
         }
-        link();
+        for (int i = 0; i < inputs.length; i++) {
+            outputs[i] = inputs[i].getReadAccess();
+        }
     }
 
     @Override
     public boolean forward() {
         boolean anyForwarded = false;
-        for (int i = 0; i < predecessors.length; i++) {
-            final SequentialNodeImp predecessor = predecessors[i];
+        for (var predecessor : predecessors) {
             if (predecessor.forward()) {
                 anyForwarded = true;
-            } else {
-                if (!exhausted[i]) {
-                    exhausted[i] = true;
-                    for (int o : predecessorOutputIndices[i]) {
-                        final DelegatingReadAccesses.DelegatingReadAccess output = outputs[o];
-                        output.setDelegateAccess(MissingAccesses.getMissingAccess(output.getDataSpec()));
-                    }
-                }
             }
         }
         return anyForwarded;
