d0bc73d8f76139ba1daf2be6d27554193a5552fe
tpietzsch <tobias.pietzsch@gmail.com>
2024-08-20 18:00:01 +0200

    WIP

--- org.knime.core.table.tests/src/test/java/org/knime/core/table/virtual/graph/rag2/RagPlayground2.java
+++ org.knime.core.table.tests/src/test/java/org/knime/core/table/virtual/graph/rag2/RagPlayground2.java
@@ -2,12 +2,25 @@ package org.knime.core.table.virtual.graph.rag2;
 
 import org.knime.core.table.virtual.VirtualTable;
 import org.knime.core.table.virtual.graph.VirtualTableTests;
+import org.knime.core.table.virtual.graph.rag2.SpecGraphBuilder.SpecGraph;
 
 public class RagPlayground2 {
 
     public static void main(String[] args) {
-        final VirtualTable table = VirtualTableTests.vtMinimal();
-        final SpecGraphBuilder.SpecGraph specGraph = SpecGraphBuilder.buildSpecGraph(table);
-        System.out.println("specGraph = " + specGraph);
+//        final VirtualTable table = VirtualTableTests.vtMinimal();
+//        final VirtualTable table = VirtualTableTests.vtAppendAndSlice();
+        final VirtualTable table = VirtualTableTests.vtMapsAndFilters();
+
+        final SpecGraph specGraph = SpecGraphBuilder.buildSpecGraph(table);
+
+        var mermaid = new Mermaid();
+
+        mermaid.append("<pre>" + specGraph + "</pre>");
+        mermaid.append("buildSpec(table)", "SPEC edges", specGraph);
+
+        mermaid.save("/Users/pietzsch/git/mermaid/a_graph.html");
+
+        System.out.println("\n\n=========================\n");
+        System.out.println("graph = " + specGraph);
     }
 }
