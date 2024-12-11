38afa26a79c99462a84e08698ea4ebe97986afd4
Tobias Kampmann <tobias.kampmann@tngtech.com>
2024-12-11 17:21:03 +0100

    wip - rounding3

--- org.knime.core.expressions/src/main/java/org/knime/core/expressions/functions/DateTimeFunctions.java
+++ org.knime.core.expressions/src/main/java/org/knime/core/expressions/functions/DateTimeFunctions.java
@@ -20,7 +20,6 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.Period;
 import java.time.ZonedDateTime;
 import java.time.chrono.Chronology;
 import java.time.format.DateTimeFormatter;
@@ -33,6 +32,7 @@ import java.util.function.Function;
 import org.knime.core.expressions.Arguments;
 import org.knime.core.expressions.Computer;
 import org.knime.core.expressions.Computer.DurationComputer;
+import org.knime.core.expressions.Computer.LocalDateComputer;
 import org.knime.core.expressions.Computer.LocalDateTimeComputer;
 import org.knime.core.expressions.Computer.LocalTimeComputer;
 import org.knime.core.expressions.Computer.PeriodComputer;
@@ -45,6 +45,7 @@ import org.knime.core.webui.node.dialog.defaultdialog.setting.interval.Interval;
 import org.knime.core.webui.node.dialog.defaultdialog.setting.interval.TimeInterval;
 import org.knime.time.node.manipulate.datetimeround.DateRoundNodeSettings;
 import org.knime.time.node.manipulate.datetimeround.DateRoundNodeSettings.RoundDatePrecision;
+import org.knime.time.node.manipulate.datetimeround.DateRoundingUtil;
 import org.knime.time.node.manipulate.datetimeround.TimeRoundNodeSettings;
 import org.knime.time.node.manipulate.datetimeround.TimeRoundingUtil;
 import org.knime.time.util.DurationPeriodFormatUtils;
@@ -153,76 +154,6 @@ public final class DateTimeFunctions {
         }
     }
 
-    /**
-     * Helper function to round time-based temporal values. Assumption: The arguments 'strategy' and 'precision' are
-     * present, where precision is a Duration.
-     *
-     * @param args arguments to the function
-     * @param temporalArgumentName name of the temporal argument to round
-     * @param ctx evaluation context
-     * @return the rounded temporal value
-     */
-    private static final Temporal roundTimeBasedTemporal(final Arguments<Computer> args,
-        final String temporalArgumentName, final EvaluationContext ctx) {
-        var temporalComputer = args.get(temporalArgumentName);
-        var strategyComputer = toString(args.get("strategy", defaultTimeRoundStrategyComputer));
-        var precisionComputer = toDuration(args.get("precision", oneHourDurationComputer));
-
-        var timeToRound = Computer.computeTemporal(temporalComputer, ctx);
-        var strategy = TimeRoundNodeSettings.TimeRoundingStrategy.valueOf(strategyComputer.compute(ctx));
-        var precision = precisionComputer.compute(ctx);
-
-        return TimeRoundingUtil.roundTimeBasedTemporal(timeToRound, strategy, precision);
-    }
-
-    /**
-     * Helper function to round time-based temporal values. Assumption: The arguments 'strategy' and 'precision' are
-     * present, where precision is a Period.
-     *
-     * @param args arguments to the function
-     * @param temporalArgumentName name of the temporal argument to round
-     * @param ctx evaluation context
-     * @return true if the temporal argument is missing or if the strategy or precision is missing
-     */
-    private static final Temporal roundDateBasedTemporal(final Arguments<Computer> args,
-        final String temporalArgumentName, final EvaluationContext ctx) {
-        var temporalComputer = args.get(temporalArgumentName);
-        var strategyComputer = toString(args.get("strategy", defaultDateRoundStrategyComputer));
-        var precisionComputer = toPeriod(args.get("precision", oneDayPeriodComputer));
-        var shiftModeComputer = toString(args.get("shift_mode", defaultTimeShiftModeComputer));
-
-        var timeToRound = Computer.computeTemporal(temporalComputer, ctx);
-        var strategy = DateRoundNodeSettings.DateRoundingStrategy.valueOf(strategyComputer.compute(ctx));
-        var precision = precisionComputer.compute(ctx);
-        var shiftMode = DateRoundNodeSettings.ShiftMode.valueOf(shiftModeComputer.compute(ctx));
-
-        return LocalDate.now();
-    }
-
-    /**
-     * Helper function to determine if the temporal argument is missing or if the strategy or precision is missing.
-     *
-     * @param args arguments to the function
-     * @param temporalArgumentName name of the temporal argument to round
-     * @param ctx evaluation context
-     * @return true if the temporal argument is missing or if the strategy or precision is missing
-     */
-    private static final boolean roundTemporalIsMissing(final Arguments<Computer> args,
-        final String temporalArgumentName, final EvaluationContext ctx) {
-        if (args.get(temporalArgumentName).isMissing(ctx)) {
-            return true;
-        }
-
-        if (args.has("strategy") && args.get("strategy").isMissing(ctx)) {
-            return true;
-        }
-
-        if (args.has("precision") && args.get("precision").isMissing(ctx)) {
-            return true;
-        }
-        return false;
-    }
-
     public static final ExpressionFunction ROUND_DATE = functionBuilder() //
         .name("round_date") //
         .description("""
@@ -237,40 +168,34 @@ public final class DateTimeFunctions {
             arg("date", "Date to round", hasTimePartOrIsOpt()), //
             optarg("precision", "Precision to round to: " + RoundDatePrecision.values(), isStringOrOpt()), //
             optarg("strategy", "Rounding strategy: 'FIRST', 'NEAREST', 'LAST'", isStringOrOpt()), //
-            optarg("shift_mode", "Shift mode: 'PREVIOUS','THIS', 'NEXT'", isStringOrOpt()) //
+            optarg("shift_mode", "Shift mode: 'PREVIOUS','THIS', 'NEXT'", isStringOrOpt()), //
+            optarg("dayOrWeekday", "If set to weekday rounding will exclude weekends", isStringOrOpt()) //
         ) //
         .returnType("A local date", RETURN_LOCAL_TIME, args -> LOCAL_TIME(anyOptional(args)))//
         .impl(DateTimeFunctions::roundDate) //
         .build();
 
     private static Computer roundDate(final Arguments<Computer> args) {
-        var dateArgument = args.get("date");
-        var strategyComputer = toString(args.get("strategy", defaultTimeRoundStrategyComputer));
-        var precisionComputer = toString(args.get("precision", oneDayPeriodComputer));
-        var shiftModeComputer = toString(args.get("shift_mode", defaultTimeRoundStrategyComputer));
+        var dateComputer = args.get("date");
 
-        if (dateArgument instanceof LocalTimeComputer dateComputer) {
-            return LocalTimeComputer.of(ctx -> {
-                var dateToRound = dateComputer.compute(ctx);
-                var strategy = TimeRoundNodeSettings.TimeRoundingStrategy.valueOf(strategyComputer.compute(ctx));
-                var precision = precisionComputer.compute(ctx);
-                var shiftMode = DateRoundNodeSettings.ShiftMode.valueOf(shiftModeComputer.compute(ctx));
-
-                return LocalTime.now();
-            }, ctx -> {
-
-                try {
-                    TimeRoundNodeSettings.TimeRoundingStrategy.valueOf(strategyComputer.compute(ctx));
-                    return false;
-                } catch (IllegalArgumentException e) {
-                    return true;
-                }
-
-            });
+        if (dateComputer instanceof LocalDateComputer) {
+            return LocalDateComputer.of( //
+                ctx -> (LocalDate)roundDateBasedTemporal(args, "date", ctx), //
+                ctx -> roundTemporalIsMissing(args, "date", ctx) //
+            );
+        } else if (dateComputer instanceof LocalDateTimeComputer) {
+            return LocalDateTimeComputer.of( //
+                ctx -> (LocalDateTime)roundDateBasedTemporal(args, "date", ctx), //
+                ctx -> roundTemporalIsMissing(args, "date", ctx) //
+            );
+        } else if (dateComputer instanceof ZonedDateTimeComputer) {
+            return ZonedDateTimeComputer.of( //
+                ctx -> (ZonedDateTime)roundDateBasedTemporal(args, "date", ctx), //
+                ctx -> roundTemporalIsMissing(args, "date", ctx) //
+            );
+        } else {
+            throw FunctionUtils.calledWithIllegalArgs();
         }
-
-        return ctx -> true;
-
     }
 
     public static final ExpressionFunction PARSE_PERIOD = functionBuilder() //
@@ -491,9 +416,86 @@ public final class DateTimeFunctions {
     private static Computer defaultTimeShiftModeComputer =
         StringComputer.of(ctx -> DateRoundNodeSettings.ShiftMode.THIS.name(), ctx -> false);
 
+    private static Computer defaultDayOrWWeekdayComputer =
+        StringComputer.of(ctx -> DateRoundNodeSettings.DayOrWeekday.DAY.name(), ctx -> false);
+
     private static Computer zeroDurationComputer = DurationComputer.of(ctx -> Duration.ZERO, ctx -> false);
 
     private static Computer oneHourDurationComputer = DurationComputer.of(ctx -> Duration.ofHours(1), ctx -> false);
 
-    private static Computer oneDayPeriodComputer = PeriodComputer.of(ctx -> Period.ofDays(1), ctx -> false);
+    private static Computer defaultDatePrecisionComputer =
+        StringComputer.of(ctx -> RoundDatePrecision.MONTH.name(), ctx -> false);
+
+    /**
+     * Helper function to round time-based temporal values. Assumption: The arguments 'strategy' and 'precision' are
+     * present, where precision is a Duration.
+     *
+     * @param args arguments to the function
+     * @param temporalArgumentName name of the temporal argument to round
+     * @param ctx evaluation context
+     * @return the rounded temporal value
+     */
+    private static final Temporal roundTimeBasedTemporal(final Arguments<Computer> args,
+        final String temporalArgumentName, final EvaluationContext ctx) {
+        var temporalComputer = args.get(temporalArgumentName);
+        var strategyComputer = toString(args.get("strategy", defaultTimeRoundStrategyComputer));
+        var precisionComputer = toDuration(args.get("precision", oneHourDurationComputer));
+
+        var timeToRound = Computer.computeTemporal(temporalComputer, ctx);
+        var strategy = TimeRoundNodeSettings.TimeRoundingStrategy.valueOf(strategyComputer.compute(ctx));
+        var precision = precisionComputer.compute(ctx);
+
+        return TimeRoundingUtil.roundTimeBasedTemporal(timeToRound, strategy, precision);
+    }
+
+    /**
+     * Helper function to round time-based temporal values. Assumption: The arguments 'strategy' and 'precision' are
+     * present, where precision is a Period.
+     *
+     * @param args arguments to the function
+     * @param temporalArgumentName name of the temporal argument to round
+     * @param ctx evaluation context
+     * @return true if the temporal argument is missing or if the strategy or precision is missing
+     */
+    private static final Temporal roundDateBasedTemporal(final Arguments<Computer> args,
+        final String temporalArgumentName, final EvaluationContext ctx) {
+
+        var temporalComputer = args.get(temporalArgumentName);
+        var strategyComputer = toString(args.get("strategy", defaultDateRoundStrategyComputer));
+        var precisionComputer = toString(args.get("precision", defaultDatePrecisionComputer));
+        var shiftModeComputer = toString(args.get("shift_mode", defaultTimeShiftModeComputer));
+        var dayOrWeekdayComputer = toString(args.get("dayOrWeekday", defaultDayOrWWeekdayComputer));
+
+        var dateToRound = Computer.computeTemporal(temporalComputer, ctx);
+        var strategy = DateRoundNodeSettings.DateRoundingStrategy.valueOf(strategyComputer.compute(ctx));
+        var precision = DateRoundNodeSettings.RoundDatePrecision.valueOf(precisionComputer.compute(ctx));
+        var shiftMode = DateRoundNodeSettings.ShiftMode.valueOf(shiftModeComputer.compute(ctx));
+        var dayOrWeekday = DateRoundNodeSettings.DayOrWeekday.valueOf(dayOrWeekdayComputer.compute(ctx));
+
+        return DateRoundingUtil.roundDateBasedTemporal(dateToRound, strategy, precision, shiftMode, dayOrWeekday);
+    }
+
+    /**
+     * Helper function to determine if the temporal argument is missing or if the strategy or precision is missing.
+     *
+     * @param args arguments to the function
+     * @param temporalArgumentName name of the temporal argument to round
+     * @param ctx evaluation context
+     * @return true if the temporal argument is missing or if the strategy or precision is missing
+     */
+    private static final boolean roundTemporalIsMissing(final Arguments<Computer> args,
+        final String temporalArgumentName, final EvaluationContext ctx) {
+        if (args.get(temporalArgumentName).isMissing(ctx)) {
+            return true;
+        }
+
+        if (args.has("strategy") && args.get("strategy").isMissing(ctx)) {
+            return true;
+        }
+
+        if (args.has("precision") && args.get("precision").isMissing(ctx)) {
+            return true;
+        }
+        return false;
+    }
 }
